b885222657ad3c98a955541057162d2f9aa6a665
kanaZukii <cosmicpizza17@gmail.com>
2025-07-15 15:47:54 +0800

    Exposing Variables to editor

---
+++ engine/src/main/java/dev/kanazukii/ether/engine/components/RigidBody.java
@@ -0,0 +1,15 @@
+package dev.kanazukii.ether.engine.components;
+
+import java.util.Vector;
+
+import org.joml.Vector3f;
+import org.joml.Vector4f;
+
+import dev.kanazukii.ether.engine.Component;
+
+public class RigidBody extends Component {
+    private int colliderType  = 0;
+    private float friction = 0.0f;
+    public Vector3f velocity = new Vector3f(0,0.5f,0);
+    public transient Vector4f temp = new Vector4f(0,0,0,0);
+}
